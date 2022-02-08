743a99c9704d40f08f566a1a6b57a032841c211a
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-02-08 14:25:06 +0000

    update port

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const helpers = require("./helpers");
 
 const SequelizeStore = connectSessionSequelize(session.Store);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 
 const sessionOptions = {
   secret: process.env.SESSION_SECRET,
