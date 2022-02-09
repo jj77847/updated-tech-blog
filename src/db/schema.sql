d9ffa40dfd4d30a2ca06ccf214c4cc008293c283
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-02-09 12:07:37 +0000

    updated schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS updated-tech_blog_db;
+DROP DATABASE IF EXISTS updated_tech_blog_db;
 
-CREATE DATABASE updated-tech_blog_db;
+CREATE DATABASE updated_tech_blog_db;
 
-USE updated-tech_blog_db;
+USE updated_tech_blog_db;
