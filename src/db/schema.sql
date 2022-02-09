1f5b841dbb319ab68067a37aa83998f4b71c4bf8
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-02-09 11:57:48 +0000

    update files

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS tech_blog_db;
+DROP DATABASE IF EXISTS updated-tech_blog_db;
 
-CREATE DATABASE tech_blog_db;
+CREATE DATABASE updated-tech_blog_db;
 
-USE tech_blog_db;
+USE updated-tech_blog_db;
